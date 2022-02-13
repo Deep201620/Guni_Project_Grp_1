49114e884185a6a15c2c3139f52a37f2b9c78062
Deep201620 <deep19731@gmail.com>
2022-02-14 00:18:09 +0530

    Added the identity pages to the project.

---
+++ GuniApp_Proj_1.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GuniApp_Proj_1.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuniApp_Proj_1.Web.Areas.Identity.IdentityHostingStartup))]
+namespace GuniApp_Proj_1.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
